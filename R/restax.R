00f6375005c9c16f82acd82b21b53228219452c0
edisz <eduardszoecs@gmail.com>
2014-06-06 23:56:47 +0200

    added data

---
+++ R/restax.R
@@ -0,0 +1,13 @@
+#' Resolving ambiguous taxa
+#' 
+#' @author Eduard Szoecs \email{eduardszoecs@@gmail.com}
+#' @docType package
+#' @name samp
+NULL
+
+#' Example data
+#' 
+#' Fictitious Example Data for restax from Cuffney et al. (2007)
+#' @source Cuffney, T. F., Bilger, M. D. & Haigler, A. M. Ambiguous taxa: effects on the characterization and interpretation of invertebrate assemblages. Journal of the North American Benthological Society 26, 286–307 (2007).
+#' @name samp
+NULL
